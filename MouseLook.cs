da46e8eaea3eb9be888b2ea1bd0ccf423da5ebd3
subhadippal66 <62802794+subhadippal66@users.noreply.github.com>
2020-09-26 16:34:07 +0530

    FPS control added
    WASD to move, mouse to move around and space to jump.

---
+++ MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] float mouseSensivity;
+    [SerializeField] Transform playerBody;
+    float yRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        yRot = yRot - mouseY;
+        yRot = Mathf.Clamp(yRot, -90, 90);
+        transform.localRotation = Quaternion.Euler(yRot, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    CharacterController chCont;
+    [SerializeField] float speed = 10f;
+
+    [SerializeField] float gravity = -9.81f;
+    Vector3 fallVelocity;
+
+    [SerializeField] Transform groundCheck;  //an empty which is in the bottom of player yelling the position to check
+    [SerializeField] float sphereRad = .4f;  //an argument of physics.checksphere
+    [SerializeField] LayerMask groundLayer;  //to specify which object to collide and that to ignore 
+    bool isGrounded;
+    [SerializeField] float jumpVelocity = 20f;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        chCont = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMove();
+        HandleGravity();
+        Jump();
+    }
+
+    public void Jump()    //handles jump
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            fallVelocity.y = jumpVelocity;
+        }
+    }
+
+    public void HandleGravity()   //this will handle gravity in the scene
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, sphereRad, groundLayer);  //needs 3 argument
+        if (isGrounded && fallVelocity.y < 0)    //check if the player is in the grolund or not
+        {
+            fallVelocity.y = 0f;
+        }
+        fallVelocity.y += gravity * Time.deltaTime * Time.deltaTime;  //physics formula
+        chCont.Move(fallVelocity);
+    }
+
+    public void HandleMove()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 moveVect = transform.forward * moveZ + transform.right * moveX;
+        chCont.Move(moveVect * speed * Time.deltaTime);
+    }
+}
